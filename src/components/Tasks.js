dfcfea3380d6e2fcbe05ad5872946b0af00586ca
Enoch Garoli <eenkiga@gmail.com>
2022-06-21 09:34:51 +0300

    SecurityDispatch

---
+++ src/components/Tasks.js
@@ -0,0 +1,19 @@
+import Task from './Task'
+import React from 'react'
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+
+  
+
+  return (
+    <>
+    {tasks.map((task, index)=>(
+    <Task key={index} task={task}
+    onDelete={onDelete}
+    onToggle={onToggle}/>
+    ))}
+    </>
+  )
+}
+
+export default Tasks
